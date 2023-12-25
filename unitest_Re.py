da9651593f57c26a035f78724cb15a905f160b03
S0A0D0Do <115955659+S0A0D0Do@users.noreply.github.com>
2023-12-25 12:37:04 +0300

    Create unitest_Re.py

---
+++ unitest_Re.py
@@ -0,0 +1,37 @@
+import unittest
+from rectangle import area as rectangle_area, perimeter as rectangle_perimeter
+
+class TestSquare(unittest.TestCase):
+	def test_zero_area(self):
+		self.assertEqual(rectangle_area(0), 0)
+
+	def test_small_area(self):
+		self.assertEqual(rectangle_area(0.345,0.45), 0.15525)
+
+	def test_big_area(self):
+		self.assertEqual(rectangle_area(123,890), 1009470)
+
+	def test_negative_numbers_area(self):
+		with self.assertRaises(ValueError):
+			rectangle_area(-1)
+
+	def test_normal_area(self):
+		self.assertEqual(srectangle_area(2,5), 10)
+
+	" Рассмоттрим периметр при этих же условиях"
+
+	def test_zero_perimeter(self):
+		self.assertEqual(rectangle_perimeter(0), 0)
+
+	def test_small_perimeter(self):
+		self.assertEqual(rectangle_perimeter(0.345,0.45),1.59)
+
+	def test_big_perimeter(self):
+		self.assertEqual(rectangle_perimeter(123,890), 2026)
+
+	def test_negative_numbers_perimeter(self):
+		with self.assertRaises(ValueError):
+			rectangle_perimeter(-1)
+
+	def test_normal_area(self):
+		self.assertEqual(rectangle_perimeter(3,25), 40)
