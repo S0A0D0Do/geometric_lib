1a8e0fd7d74b1c1dcbfd89163c58048bd9dae69f
S0A0D0Do <115955659+S0A0D0Do@users.noreply.github.com>
2023-12-25 15:44:27 +0300

    Update unitest_Tr.py

--- unitest_Tr.py
+++ unitest_Tr.py
@@ -26,4 +26,4 @@ class TestSquare(unittest.TestCase):
 		self.assertEqual(square_perimeter(543), 2172)
 
 	def test_normal_perimeter(self):
-		self.assertEqual(square_area(10), 40)
+		self.assertEqual(square_perimeter(10), 40)
